6bdc9d033771f90b2e512801a194eb43909839dc
Artem Balanovsky <mr.balanovsky@gmail.com>
2022-09-23 16:05:32 +0300

    Review e2e testes created

--- src/product/product.model.ts
+++ src/product/product.model.ts
@@ -11,9 +11,7 @@ class ProductCharacteristic {
 
 export interface ProductModel extends Base {}
 export class ProductModel extends TimeStamps{
-	@prop()
-	_id: string;
-
+	
 	@prop()
 	image: string;
 
