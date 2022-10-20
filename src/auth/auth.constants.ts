da93c473667e34b3acf45aa406763741f43556ca
Artem Balanovsky <mr.balanovsky@gmail.com>
2022-10-20 20:01:13 +0300

    jwt guard added

---
+++ src/auth/auth.constants.ts
@@ -0,0 +1,3 @@
+export const ALREADY_REGISTERED_ERROR = 'This user has been already registered before';
+export const USER_NOT_FOUND_ERROR = 'The user with this email is not found';
+export const WRONG_PASSWORD_ERROR = 'The password is not correct';
